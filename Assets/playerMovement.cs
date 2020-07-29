7a01a3031a2652a7d5a26120eb11d029f3a28ac0
siddant <siddantvardey@gmail.com>
2020-07-29 19:33:42 +0530

    Initial work on game

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class script1 : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sideForce = 500f;
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        
+        
+    }*/
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sideForce*Time.deltaTime,0,0);
+        }
+        if(Input.GetKey("a"))
+        {
+            rb.AddForce(-sideForce*Time.deltaTime,0,0);
+        }
+        if(Input.GetKey("w"))
+        {
+            rb.AddForce(0,0,forwardForce*Time.deltaTime);
+        }
+        if(Input.GetKey("s"))
+        {
+            rb.AddForce(0,0,-forwardForce*Time.deltaTime);
+        }
+    }
+}
